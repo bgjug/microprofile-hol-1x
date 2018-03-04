e9a2e2629e1dffd08ed7d082eec6f67322e31d9c
John D. Ament <john.ament@spartasystems.com>
2018-03-04 08:33:19 -0500

    Making Hammock work.

--- solution/authors/src/main/java/bg/jug/microprofile/hol/authors/Application.java
+++ solution/authors/src/main/java/bg/jug/microprofile/hol/authors/Application.java
@@ -3,5 +3,6 @@ package bg.jug.microprofile.hol.authors;
 import javax.ws.rs.ApplicationPath;
 
 @ApplicationPath("/authors")
+@javax.enterprise.context.ApplicationScoped
 public class Application extends javax.ws.rs.core.Application {
 }
