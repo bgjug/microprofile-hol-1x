e3640313a9553b308522cd1a141b11175302e32b
arjantijms <arjan.tijms@gmail.com>
2018-03-16 20:21:39 +0100

    Some fixes for JWT on Payara and in general

--- solution/subscribers/src/main/java/bg/jug/microprofile/hol/subscribers/Application.java
+++ solution/subscribers/src/main/java/bg/jug/microprofile/hol/subscribers/Application.java
@@ -2,6 +2,9 @@ package bg.jug.microprofile.hol.subscribers;
 
 import javax.ws.rs.ApplicationPath;
 
+import org.eclipse.microprofile.auth.LoginConfig;
+
 @ApplicationPath("/")
+@LoginConfig(authMethod = "MP-JWT")
 public class Application extends javax.ws.rs.core.Application {
 }
