86ed97c3f0cff5a85c8cccb4274f17519b25483c
Ivan St. Ivanov <ivan.st.ivanov@gmail.com>
2018-10-26 00:06:19 +0300

    The authentication section of the JWT chapter

--- solution/gui/src/main/java/bg/jug/microprofile/hol/gui/LoggedUserFilter.java
+++ solution/gui/src/main/java/bg/jug/microprofile/hol/gui/LoggedUserFilter.java
@@ -1,51 +1,29 @@
 package bg.jug.microprofile.hol.gui;
 
-import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.Cookie;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Stream;
 
 import static bg.jug.microprofile.hol.gui.GUIResource.AUTH_COOKIE;
 
-@WebFilter(urlPatterns = Application.RESOURCE_PATH + "/*")
-public class LoggedUserFilter implements Filter {
+@Provider
+@PreMatching
+public class LoggedUserFilter implements ContainerRequestFilter {
 
     private static final List<String> WHITE_LIST = Arrays.asList("login", "register");
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
+    public void filter(ContainerRequestContext containerRequestContext) {
+        String reqURI = containerRequestContext.getUriInfo().getPath();
+        Cookie authCookie = containerRequestContext.getCookies().get(AUTH_COOKIE);
 
-    }
-
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        HttpServletResponse httpResponse = (HttpServletResponse) response;
-
-        String reqURI = httpRequest.getRequestURI();
-
-        Cookie[] cookies = httpRequest.getCookies();
-        Optional<Cookie> authCookie =
-                cookies == null ?
-                        Optional.empty() :
-                        Stream.of(cookies)
-                              .filter(c -> c.getName().equals(AUTH_COOKIE))
-                              .findAny();
-        if (WHITE_LIST.stream().anyMatch(reqURI.toLowerCase()::contains) || authCookie.isPresent()) {
-            chain.doFilter(request, response);
-        } else {
-            httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        if (authCookie == null && WHITE_LIST.stream().noneMatch(reqURI.toLowerCase()::contains)) {
+            containerRequestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
         }
-    }
-
-    @Override
-    public void destroy() {
-
     }
 }
